11d27854a370c368f7c01bccb805f468c2010327
Sudan Basyal <sudanbasyal74@gmail.com>
2024-09-12 21:01:01 +0545

    recursion exercise on factorial and fibonacci series

---
+++ fibonacci_series.js
@@ -0,0 +1,32 @@
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+//displays the nth number in the fibonacci series
+console.log(fibonacci(5)); // 8
+
+function fibonacciSequence(n) {
+  let sequence = [];
+
+  // Handle base cases
+  if (n >= 1) {
+    sequence.push(0);
+  }
+  if (n >= 2) {
+    sequence.push(1);
+  }
+
+  // Generate the sequence iteratively
+  for (let i = 2; i < n; i++) {
+    console.log(sequence[i - 1], sequence[i - 2]);
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+
+  return sequence;
+}
+
+// Example: Display Fibonacci sequence up to n = 6
+console.log(fibonacciSequence(6)); // [0, 1, 1, 2, 3, 5]
