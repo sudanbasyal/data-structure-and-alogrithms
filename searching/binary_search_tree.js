d6ec33b0140747637a1ed71eb4f3e497f9969cb1
Sudan Basyal <sudanbasyal74@gmail.com>
2024-09-14 15:32:10 +0545

    implemented binary search tree with insert,delete,searching and finding minimum value of tree

---
+++ searching/binary_search_tree.js
@@ -0,0 +1,127 @@
+class TreeNode {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left_child = left;
+    this.right_child = right;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(data) {
+    let newNode = new TreeNode(data);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let current_node = this.root;
+      while (true) {
+        if (data < current_node.data) {
+          if (current_node.left_child === null) {
+            current_node.left_child = newNode;
+            return;
+          } else {
+            current_node = current_node.left_child;
+          }
+        } else if (data > current_node.data) {
+          if (current_node.right_child === null) {
+            current_node.right_child = newNode;
+            return;
+          } else {
+            current_node = current_node.right_child;
+          }
+        } else {
+          return;
+        }
+      }
+    }
+  }
+  search(data) {
+    let current_node = this.root;
+    while (current_node) {
+      if (current_node.data === data) {
+        return true;
+      } else if (data < current_node.data) {
+        current_node = current_node.left_child;
+      } else {
+        current_node = current_node.right_child;
+      }
+    }
+    return false;
+  }
+  delete(data) {
+    this.root = this.deleteNode(this.root, data);
+  }
+  deleteNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    // Search for the node to be deleted
+    if (data < node.data) {
+      node.left_child = this.deleteNode(node.left_child, data); // Correctly assign the updated left child
+    } else if (data > node.data) {
+      node.right_child = this.deleteNode(node.right_child, data); // Correctly assign the updated right child
+    } else {
+      // Node found
+      if (node.left_child === null && node.right_child === null) {
+        return null; // Case 1: No children (leaf node)
+      } else if (node.left_child === null) {
+        return node.right_child; // Case 2: One child (right)
+      } else if (node.right_child === null) {
+        return node.left_child; // Case 2: One child (left)
+      } else {
+        // Case 3: Two children
+        const successor = this.minValue(node.right_child); // Find the successor
+        node.data = successor.data; // Replace data with successor's data
+        node.right_child = this.deleteNode(node.right_child, successor.data); // Delete the successor
+      }
+    }
+    return node; // Return the updated node
+  }
+
+  minValue(node) {
+    let current_node = node;
+    while (current_node.left_child) {
+      current_node = current_node.left_child;
+    }
+    return current_node;
+  }
+  minValueOfTree() {
+    let current = this.root;
+
+    while (current.left_child) {
+      current = current.left_child;
+    }
+    return current.data;
+  }
+}
+
+// usage of the binary search tree
+
+let bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.insert(13);
+bst.insert(17);
+bst.insert(12);
+bst.insert(14);
+bst.insert(16);
+bst.insert(18);
+
+console.log(bst);
+
+console.log(bst.search(13)); // true
+console.log(bst.search(11)); // false
+
+bst.delete(13);
+console.log(bst.search(13)); // false
+console.log(bst);
+console.log("minimum_value", bst.minValueOfTree()); // 3
+
+bst.delete(15);
+console.log(bst);
